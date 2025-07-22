e6f3c50729b1b92bf18fe34f3672031edaa0763f
Student <Student@Train11-3-PC174-L.control.ad.sit.kmutt.ac.th>
2025-07-22 16:15:50 +0700

    add BasicP_class_2.py and BasicP_class2_workshop.py

---
+++ BasicP_class_2.py
@@ -0,0 +1,47 @@
+# x = "That's something"
+# print("ค่าในตัวแปร x:", x)
+# y = 20.0
+# b = False
+# print("ค่าในตัวแปร y:", y)
+# print("Type ของ x:", type(x))
+# print("Type ของ b:", type(b))
+# print("Type ของ y:", type(y))
+# num = "10.0"
+# print("ค่าของ num: ", num)
+# print("Type ของ num: ", type(num))
+# print("หลังจากแปลงค่า")
+# print("Type ของ num: ", type(float(num)))
+# mod = 10 % 4
+# print(mod)
+# ten = 10
+# one = 1
+# print()
+# T = True
+# F = False
+# print( not T or F)
+# num1 = int(input("กรอกเลข1: "))
+# num2 = int(input("กรอกเลข2: "))
+# print("num1 =", num1)
+# print("num2 =", num2)
+# print("type num =", type(num1))
+# print("type num =", type(num2))
+# print(num1 > num2)
+
+x = 3
+y = 5
+if x == y:
+    print("inside if:", "x == y")
+elif x > y:
+    print("inside elif:", "x > y")
+elif x < y:
+    print("inside elif: ", "x < y")
+else:
+    print("inside else:", "x != y")
+print("outside if:", "staterpack Yai Mak")
+
+T = True
+F = True
+if T and F:
+    print("inside if: False")
+else:
+    print("inside else: True")
