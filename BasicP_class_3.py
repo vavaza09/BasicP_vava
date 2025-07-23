8283c280c54c3c937b6a6512cabdccd77ce58c1d
Student <Student@Train11-3-PC174-L.control.ad.sit.kmutt.ac.th>
2025-07-23 12:24:26 +0700

    add class_3

---
+++ BasicP_class_3.py
@@ -0,0 +1,59 @@
+# has_rice = True
+# has_spoon = False
+
+# if has_rice == True:
+#     print("ิมีข้าวแล้ว")
+#     if has_spoon:
+#         print("มีช้อน กินข้าวได้")
+#     else:
+#         print("ไม่มีช้อน กินไม่ได้")
+# else:
+#     print("ไม่มีข้าว")
+
+# if has_rice == True and has_spoon == True:
+#     print("มีช้อน มีข้าว กินข้าวได้")
+# else:
+#     print("ไม่มีข้าวหรือไม่มีช้อน กินข้าวไม่ได้")
+# b = True
+# for index in range(1, 6):
+#     print("Round:", index, "Hello World")
+
+#     if index == 2:
+#         print("55555")
+
+# ถามเลขจากผู้ใช้
+# อยากลบกี่รอบ
+
+# ถามว่าจะลบด้วยเลขอะไร
+# จากนั้นนำเลขจากผู้ใช้มาลบ ด้วย เลขที่ถาม
+
+# num = int(input("Enter number: "))
+# times = int(input("Enter times: "))
+
+# for i in range(times):
+#     x = 10
+#     print(x-2)
+#     minus_num = int(input())
+#     num = num - minus_num
+#     print("เหลือ", num)
+
+# สมมติเลขเป็น 100
+# อยากจะลบสัก 2 รอบ
+
+# รอบ 1 ลบด้วยเลข 10
+# print ออกมาว่า เหลือเท่าไหร่
+
+# รอบ 2 ลบด้วยเลข 5
+# print ออกมาว่า เหลือเท่าไหร่
+
+# เมื่อจบ loop print ผลลัพธ์สุดท้ายของตัวเลข
+
+
+# ถ้าเลือก 1 จะให้เลือกใหม่
+# ถ้าเลือก 2 จะจบการทำงาน
+# while True:
+#     option = input("Enter number: ")
+#     if option == "1":
+#         print("try again")
+#     elif option == "2":
+#         break
